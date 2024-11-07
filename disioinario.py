555f063875913edf81eb14cffdd56620aed67506
kamilo-840 <ccorvalan@escuelasproa.edu.ar>
2024-11-07 09:13:28 -0300

    Add files via upload

---
+++ disioinario.py
@@ -0,0 +1,4 @@
+nombrecompleto={"nombre":("camilo","corvalan"),"edad":14,"direccion":"av. cacique tulian 250","actividades favoritas":["rugby", "voley", "gym", "salir con amigos"]}
+for c,v in nombrecompleto.items():
+    print (c, ":", v)
+    print("........................")
